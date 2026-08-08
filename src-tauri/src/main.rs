12f152c5b73b3132411aa735f8d2d50a791040d2
sumitbihsnoi9833 <sumit989bishnoi@gmail.com>
2026-08-08 23:13:13 +0530

    Fix: Use tauri-plugin-single-instance plugin correctly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,6 @@
 
 use tauri::{Manager, AppHandle, http::Response};
 use tauri_plugin_global_shortcut::{Shortcut, GlobalShortcutExt};
-use tauri_plugin_single_instance::SingleInstanceExt;
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
@@ -166,6 +165,13 @@ fn main() {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+                let _ = window.show();
+                let _ = window.unminimize();
+            }
+        }))
         .manage(settings_state)
         .manage(bookmarks_state)
         .invoke_handler(tauri::generate_handler![
@@ -183,15 +189,6 @@ fn main() {
             window_close,
         ])
         .setup(|app| {
-            // Single instance - focus existing window on second launch
-            let _ = app.handle().plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-                if let Some(window) = app.get_webview_window("main") {
-                    let _ = window.set_focus();
-                    let _ = window.show();
-                    let _ = window.unminimize();
-                }
-            }));
-
             let main_window = app.get_webview_window("main").unwrap();
             main_window.show().unwrap();
             main_window.set_focus().unwrap();
